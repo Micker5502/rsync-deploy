2d93ae4362666594c0d4addafeb55f9f4b2bc95d
Micker5502 <zotto5502@hotmail.com>
2021-11-12 16:32:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ mkdir -p "$SSHPATH"
 echo "$DEPLOY_KEY" > "$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
-mkdir -p $HOME/$PROJECT_NAME
 cp -r $GITHUB_WORKSPACE/ $HOME/$PROJECT_NAME
 # sync it up"
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $HOME/$PROJECT_NAME$FOLDER $SERVER_DEPLOY_STRING"
